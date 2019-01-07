5a8ec239d333566521cca59dc745f35e7175953c
Marte Julie <m.j.satra@fys.uio.no>
2019-01-07 17:25:36 +0100

    test conductivity_k

--- tests/test_leakycell.py
+++ tests/test_leakycell.py
@@ -3,7 +3,6 @@ from pinsky_rinzel_pump.leakycell import *
 
 def test_modules():
     test_cell = LeakyCell(279.3, 15., 145., 15., 145., 100., 3., 100., 3., 115., 148., 115., 148.)
-
     assert round(test_cell.nernst_potential(1., 400., 20.), 4) == -0.0721
 
 #def test_I_diff():
@@ -12,10 +11,7 @@ def test_modules():
 #    k_d = [14, 114]
 #    k_s = [16, 116]
 #    assert round(I_diff(3.2, D_k, Z_k, k_d, k_s), 3) == 1.319
-#
-#def test_sigma():
-#    D_k = [1.33e-9, 2.03e-9]
-#    Z_k = [1, -1]
-#    k_d = [14, 114]
-#    k_s = [16, 116]
-#    assert round(sigma(3.2, D_k, Z_k, k_d, k_s), 3) == 0.099
+
+def test_conductivity_k():
+    test_cell = LeakyCell(279.3, 14., 145., 16., 145., 100., 3., 100., 3., 115., 148., 115., 148.)
+    assert round(test_cell.conductivity_k(test_cell.D_Na, test_cell.Z_Na, 3.2, test_cell.Na_si, test_cell.Na_di), 4) == 0.0078
