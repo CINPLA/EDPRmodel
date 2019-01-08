b69e4fdfd6e7b700bb92974f7cbfa3350d478d18
Marte Julie <m.j.satra@fys.uio.no>
2019-01-08 16:22:24 +0100

    test charge symmetry when initial [k] values give rise to electrodiffusion, test failed!

--- tests/test_leakycell.py
+++ tests/test_leakycell.py
@@ -113,7 +113,7 @@ def test_charge_symmetry():
         assert round(q_di[i], 5) == -round(q_de[i], 5)
 
 def test_charge_conservation_w_diffusion():
-    """Tests that no charge disappear when ion diffuse."""
+    """Tests that no charge disappear when ions diffuse."""
     
     EPS = 1e-14
 
@@ -153,3 +153,45 @@ def test_charge_conservation_w_diffusion():
         assert Na_diff_charge[i] < EPS
         assert K_diff_charge[i] < EPS
         assert Cl_diff_charge[i] < EPS
+
+def test_charge_symmetry_w_diffusion():
+    """Tests that Q_i = -Q_e when ions diffuse."""
+
+    Na_si0 = 12.
+    Na_se0 = 142.
+    K_si0 = 99.
+    K_se0 = 2.
+    Cl_si0 = 111.
+    Cl_se0 = 144.
+    Na_di0 = 18.
+    Na_de0 = 148.
+    K_di0 = 101.
+    K_de0 = 4.
+    Cl_di0 = 119.
+    Cl_de0 = 152.
+
+    t_span = (0, 100)
+    k0 = [Na_si0, Na_se0, Na_di0, Na_de0, K_si0, K_se0, K_di0, K_de0, Cl_si0, Cl_se0, Cl_di0, Cl_de0]
+    sol = solve_ivp(dkdt, t_span, k0, max_step=0.001)
+
+    Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de = sol.y
+    t = sol.t
+    
+    test_cell = LeakyCell(279.3, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, \
+        Cl_si, Cl_se, Cl_di, Cl_de)
+
+    q_si = test_cell.total_charge([test_cell.Na_si, test_cell.K_si, test_cell.Cl_si], \
+        test_cell.V_si)
+    q_se = test_cell.total_charge([test_cell.Na_se, test_cell.K_se, test_cell.Cl_se], \
+        test_cell.V_se)
+    
+    q_di = test_cell.total_charge([test_cell.Na_di, test_cell.K_di, test_cell.Cl_di], \
+        test_cell.V_di)
+    q_de = test_cell.total_charge([test_cell.Na_de, test_cell.K_de, test_cell.Cl_de], \
+        test_cell.V_de)
+
+    N = len(q_si)
+    for i in range(N):
+        assert round(q_si[i], 5) == -round(q_se[i], 5)
+        assert round(q_di[i], 5) == -round(q_de[i], 5)
+
