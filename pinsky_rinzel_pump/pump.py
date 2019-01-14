933fe3bdadebcc65eac80bc69961d1b3e5ae8cd9
Marte Julie <m.j.satra@fys.uio.no>
2019-01-14 14:01:19 +0100

    j_pump

--- pinsky_rinzel_pump/pump.py
+++ pinsky_rinzel_pump/pump.py
@@ -1,6 +1,20 @@
+from numpy import exp
 
-
-class Pump(Leakycell):
-    """A two plus two compartment cell model with Na, K and Cl leak currents and pumps.
+class Pump(LeakyCell):
+    """A two plus two compartment cell model with Na, K and Cl leak currents, and pumps.
 
     """
+
+    def __init__(self, T, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de):
+        LeakyCell.__init__(self, T, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de)
+        self.rho = 
+        self.Ukcc2 = 
+        self.Unkcc1 =
+
+        def j_pump(self, Na_i, K_e):
+            j = self.rho / (1. + exp((25. - Na_i)/3.)) / (1. + exp(3.5 - K_e)) 
+
+        def j_Na_s(self, phi_sm, E_Na_s, Na_i, K_e):
+            j = LeakyCell.j_Na_s(self, phi_sm, E_Na_s) + 3*self.j_pump(Na_i, K_e)
+            return j
+        
