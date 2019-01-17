2f3140488df79372c3c802e9be4bb13ae89d1c21
Marte Julie <m.j.satra@fys.uio.no>
2019-01-17 11:00:04 +0100

    updated tests to deal with rest charge

--- tests/test_leakycell.py
+++ tests/test_leakycell.py
@@ -7,7 +7,7 @@ def dkdt(t,k):
     Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de = k
 
     my_cell = LeakyCell(279.3, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, \
-        Cl_si, Cl_se, Cl_di, Cl_de)
+        Cl_si, Cl_se, Cl_di, Cl_de, 0, 0, 0, 0)
 
     dNadt_si, dNadt_se, dNadt_di, dNadt_de, dKdt_si, dKdt_se, dKdt_di, dKdt_de, \
         dCldt_si, dCldt_se, dCldt_di, dCldt_de = my_cell.dkdt()
@@ -17,17 +17,20 @@ def dkdt(t,k):
 
 def test_modules():
 
-    test_cell = LeakyCell(279.3, 14., 145., 16., 145., 100., 3., 100., 3., 115., 148., 115., 148.)
+    test_cell = LeakyCell(279.3, 14., 145., 16., 145., 100., 3., 100., 3., 115., 148., 115., 148., 0, 0, 0, 0)
 
     assert round(test_cell.nernst_potential(1., 400., 20.), 4) == -0.0721
 
     assert round(test_cell.conductivity_k(test_cell.D_Na, test_cell.Z_Na, 3.2, test_cell.Na_si, test_cell.Na_di), 4) == 0.0078
 
     assert test_cell.total_charge( \
-        [test_cell.Na_si, test_cell.K_si, test_cell.Cl_si], 1e-1) == -9648
+        [test_cell.Na_si, test_cell.K_si, test_cell.Cl_si], 0, 1e-1) == -9648
     
     assert test_cell.total_charge( \
-        [10, 10, 20], 10) == 0 
+        [10, 10, 20], 0, 10) == 0 
+
+    assert test_cell.total_charge( \
+        [10, 10, 10], -10, 10) == 0 
 
 def test_charge_conservation():
     """Tests that no charge disappear."""
@@ -55,12 +58,12 @@ def test_charge_conservation():
     t = sol.t
     
     test_cell = LeakyCell(279.3, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, \
-        Cl_si, Cl_se, Cl_di, Cl_de)
+        Cl_si, Cl_se, Cl_di, Cl_de, 0, 0, 0, 0)
 
-    q_si = test_cell.total_charge([test_cell.Na_si[-1], test_cell.K_si[-1], test_cell.Cl_si[-1]], test_cell.V_si)
-    q_se = test_cell.total_charge([test_cell.Na_se[-1], test_cell.K_se[-1], test_cell.Cl_se[-1]], test_cell.V_se)        
-    q_di = test_cell.total_charge([test_cell.Na_di[-1], test_cell.K_di[-1], test_cell.Cl_di[-1]], test_cell.V_di)
-    q_de = test_cell.total_charge([test_cell.Na_de[-1], test_cell.K_de[-1], test_cell.Cl_de[-1]], test_cell.V_de)
+    q_si = test_cell.total_charge([test_cell.Na_si[-1], test_cell.K_si[-1], test_cell.Cl_si[-1]], 0, test_cell.V_si)
+    q_se = test_cell.total_charge([test_cell.Na_se[-1], test_cell.K_se[-1], test_cell.Cl_se[-1]], 0, test_cell.V_se)        
+    q_di = test_cell.total_charge([test_cell.Na_di[-1], test_cell.K_di[-1], test_cell.Cl_di[-1]], 0, test_cell.V_di)
+    q_de = test_cell.total_charge([test_cell.Na_de[-1], test_cell.K_de[-1], test_cell.Cl_de[-1]], 0, test_cell.V_de)
 
     total_q = q_si + q_se + q_di + q_de
 
@@ -92,12 +95,12 @@ def test_charge_conservation_w_diffusion():
     t = sol.t
     
     test_cell = LeakyCell(279.3, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, \
-        Cl_si, Cl_se, Cl_di, Cl_de)
+        Cl_si, Cl_se, Cl_di, Cl_de, 0, 0, 0, 0)
 
-    q_si = test_cell.total_charge([test_cell.Na_si[-1], test_cell.K_si[-1], test_cell.Cl_si[-1]], test_cell.V_si)
-    q_se = test_cell.total_charge([test_cell.Na_se[-1], test_cell.K_se[-1], test_cell.Cl_se[-1]], test_cell.V_se)        
-    q_di = test_cell.total_charge([test_cell.Na_di[-1], test_cell.K_di[-1], test_cell.Cl_di[-1]], test_cell.V_di)
-    q_de = test_cell.total_charge([test_cell.Na_de[-1], test_cell.K_de[-1], test_cell.Cl_de[-1]], test_cell.V_de)
+    q_si = test_cell.total_charge([test_cell.Na_si[-1], test_cell.K_si[-1], test_cell.Cl_si[-1]], 0, test_cell.V_si)
+    q_se = test_cell.total_charge([test_cell.Na_se[-1], test_cell.K_se[-1], test_cell.Cl_se[-1]], 0, test_cell.V_se)        
+    q_di = test_cell.total_charge([test_cell.Na_di[-1], test_cell.K_di[-1], test_cell.Cl_di[-1]], 0, test_cell.V_di)
+    q_de = test_cell.total_charge([test_cell.Na_de[-1], test_cell.K_de[-1], test_cell.Cl_de[-1]], 0, test_cell.V_de)
 
     total_q = abs(q_si + q_se + q_di + q_de)
 
