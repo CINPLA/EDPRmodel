4d6e49143acfb2a39c2488e6ca6eacf14721e8ab
Marte Julie <m.j.satra@fys.uio.no>
2019-01-28 15:31:02 +0100

    self added to all functions

--- pinsky_rinzel_pump/pinskyrinzel.py
+++ pinsky_rinzel_pump/pinskyrinzel.py
@@ -23,45 +23,45 @@ class PinskyRinzel(Pump):
         self.g_AHP = 8.
         self.g_C = 150.
 
-    def alpha_m(phi_sm):
+    def alpha_m(self, phi_sm):
         phi_1 = phi_sm*1e3 + 46.9
         alpha = - 0.32 * phi_1 / (exp(-phi_1 / 4) - 1.)
         return alpha
 
-    def beta_m(phi_sm):
+    def beta_m(self, phi_sm):
         phi_2 = phi_sm*1e3 + 19.9
         beta = 0.28 * phi_2 / (exp(phi_2 / 5.) - 1.)
         return beta
 
-    def alpha_h(phi_sm):
+    def alpha_h(self, phi_sm):
         alpha = 0.128 * exp((-43. - phi_sm*1e3) / 18.)
         return alpha
 
-    def beta_h(phi_sm):
+    def beta_h(self, phi_sm):
         phi_5 = phi_sm*1e3 + 20.
         beta = 4. / (1 + exp(-phi_5 / 5.))
         return beta
 
-    def alpha_n(phi_sm):
+    def alpha_n(self, phi_sm):
         phi_3 = phi_sm*1e3 + 24.9
         alpha = - 0.016 * phi_3 / (exp(-phi_3 / 5.) - 1)
         return alpha
 
-    def beta_n(phi_sm):
+    def beta_n(self, phi_sm):
         phi_4 = phi_sm*1e3 + 40.
         beta = 0.25 * exp(-phi_4 / 40.)
         return beta
 
-    def alpha_s(phi_dm):
+    def alpha_s(self, phi_dm):
         alpha = 1.6 / (1 + exp(-0.072 * (phi_dm*1e3 - 5.)))
         return alpha
 
-    def beta_s(phi_dm):
+    def beta_s(self, phi_dm):
         phi_6 = phi_dm*1e3 + 8.9
         beta = 0.02 * phi_6 / (exp(phi_6 / 5.) - 1.)
         return beta
 
-    def alpha_c(phi_dm):
+    def alpha_c(self, phi_dm):
         phi_7 = phi_dm*1e3 + 53.5
         phi_8 = phi_dm*1e3 + 50.0
         if phi_dm*1e3 <= -10:
@@ -70,7 +70,7 @@ class PinskyRinzel(Pump):
             alpha = 2 * exp(-phi_7 / 27.)
         return alpha
 
-    def beta_c(phi_dm):
+    def beta_c(self, phi_dm):
         phi_7 = phi_dm*1e3 + 53.5
         if phi_dm*1e3 <= -10:
             beta = 2. * exp(-phi_7 / 27.) - alpha_c(phi_dm)
@@ -78,13 +78,14 @@ class PinskyRinzel(Pump):
             beta = 0.
         return beta
 
-    def chi(Ca):
+    def chi(self, Ca):
         return min(Ca/2.5e-4, 1.0)
 
-    def alpha_q(Ca):
+    def alpha_q(self, Ca):
         return min(20*Ca, 0.01)
 
-    def beta_q():
+    def beta_q(self):
         return 0.001
 
-
+#    def j_Na_s(self,):
+#        j = Pump.j_Na
