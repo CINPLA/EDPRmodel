d24a90295acfa08cb72969c221f5768505ef04b6
Marte Julie <m.j.satra@fys.uio.no>
2019-02-15 19:05:11 +0100

    removed blank lines

--- pinsky_rinzel_pump/somatic_injection_current_K.py
+++ pinsky_rinzel_pump/somatic_injection_current_K.py
@@ -1,7 +1,4 @@
-
-
 def somatic_injection_current_K(neuron, I_stim):
 
     neuron.dKdt_si += I_stim / (neuron.V_si * neuron.F * neuron.Z_K)
     neuron.dKdt_se -= I_stim / (neuron.V_se * neuron.F * neuron.Z_K)
-
