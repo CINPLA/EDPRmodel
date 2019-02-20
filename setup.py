ac13b37c26b7465c00d0e3eb3d868a4e206474dd
Marte Julie <m.j.satra@fys.uio.no>
2019-02-20 10:03:36 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
-import os
 from setuptools import find_packages
-
-install_requires = []
 
 setup(
     name="pinsky_rinzel_pump",
     packages=find_packages(),
+    version="0.1dev"
     include_package_data=True,
-    version="0.0.1"
 )
