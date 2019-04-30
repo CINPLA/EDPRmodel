e81598d238a1575fab6e9898649ea1313c91e63b
Marte Julie <m.j.satra@fys.uio.no>
2019-04-30 13:37:19 +0200

    added comments

--- sensitivity_analysis/first_sensitivity_analysis.py
+++ sensitivity_analysis/first_sensitivity_analysis.py
@@ -2,15 +2,24 @@ import uncertainpy as un
 import chaospy as cp
 from leakwrapper import leak_wrap
 
+# Initialize the model and add labeøs
 model = un.Model(run=leak_wrap, labels=["Time (s)", "Membrane potential, soma (V)"])
 
+# Define a parameter dictionary
 parameters = {"g_Na": 0.247,
               "g_K": 0.5,
               "g_Cl": 1.0}
 
+# Create the parameters
 parameters = un.Parameters(parameters)
+
+# Set all parameters to have a uniform distribution
+# within a 20% interval around their fixed value
 parameters.set_all_distributions(un.uniform(0.2))
 
+# Perform the uncertainty quantification
 UQ = un.UncertaintyQuantification(model, parameters=parameters)
+
+# We set the seed to easier be able to reproduce the result
 data = UQ.quantify(seed=10)
 
--- sensitivity_analysis/first_sensitivity_analysis_with_features.py
+++ sensitivity_analysis/first_sensitivity_analysis_with_features.py
@@ -2,16 +2,26 @@ import uncertainpy as un
 import chaospy as cp
 from pinskyrinzelwrapper import pinskyrinzel
 
+# Initialize the model and add labels
 model = un.Model(run=pinskyrinzel, labels=["Time (s)", "Membrane potential (V)"], interpolate=True)
 
+# Define a parameter dictionary
 parameters = {"g_Na": 300,
               "g_DR": 150}
 
+# Create the parameters
 parameters = un.Parameters(parameters)
+
+# Set all parameters to have a uniform distribution 
+# within a 20 % interval around their fixed value
 parameters.set_all_distributions(un.uniform(0.2))
 
+# Initialize the features
 features = un.SpikingFeatures(features_to_run="all", threshold=-0.03, end_threshold=-0.01)
 
+# Perform the uncertainty quantification
 UQ = un.UncertaintyQuantification(model, parameters=parameters, features=features)
+
+# We set the seed to easier be able to reproduce the result
 data = UQ.quantify(seed=10)
 
